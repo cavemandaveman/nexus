24056db87c1027815449fe432a421dc0f319ee95
cavemandaveman <cavemandaveman@openmailbox.org>
2016-11-07 10:53:23 -0600

    Fix sed mistakes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 set -x
 
@@ -10,19 +10,23 @@ if [ "$1" == 'bin/nexus' ]; then
       -e "s|OBF.*|${JKS_PASSWORD}</Set>|g" \
       -i "/opt/nexus-${NEXUS_VERSION}/etc/jetty-https.xml"
     sed \
-      -e "\|application-port|a\application-port-ssl=8443" \
-      -e "\|^nexus-args| s|$|,\${karaf.etc}/jetty-https.xml,\${karaf.etc}/jetty-http-redirect-to-https.xml|" \
+      -e "s|nexus-args=.*|nexus-args=${karaf.etc}/jetty.xml,${karaf.etc}/jetty-http.xml,${karaf.etc}/jetty-requestlog.xml|g"
       -i "/opt/nexus-${NEXUS_VERSION}/etc/org.sonatype.nexus.cfg"
+    grep \
+      -q "application-port-ssl" "/opt/nexus-${NEXUS_VERSION}/etc/org.sonatype.nexus.cfg" || \
+      sed \
+        -e "\|application-port|a\application-port-ssl=8443" \
+        -i "/opt/nexus-${NEXUS_VERSION}/etc/org.sonatype.nexus.cfg"
   fi
   sed \
-    -e "s|-Xms1200M|-Xms${JAVA_MIN_MEM}|g" \
-    -e "s|-Xmx1200M|-Xmx${JAVA_MAX_MEM}|g" \
-    -e "s|karaf.home=.|karaf.home=/opt/nexus-${NEXUS_VERSION}|g" \
-    -e "s|karaf.base=.|karaf.base=/opt/nexus-${NEXUS_VERSION}|g" \
-    -e "s|karaf.etc=etc|karaf.etc=/opt/nexus-${NEXUS_VERSION}/etc|g" \
-    -e "s|java.util.logging.config.file=etc|java.util.logging.config.file=/opt/nexus-${NEXUS_VERSION}/etc|g" \
-    -e "s|karaf.data=data|karaf.data=${NEXUS_DATA}|g" \
-    -e "s|java.io.tmpdir=data/tmp|java.io.tmpdir=${NEXUS_DATA}/tmp|g" \
+    -e "s|-Xms.*|-Xms${JAVA_MIN_MEM}|g" \
+    -e "s|-Xmx.*|-Xmx${JAVA_MAX_MEM}|g" \
+    -e "s|karaf.home=.*|karaf.home=/opt/nexus-${NEXUS_VERSION}|g" \
+    -e "s|karaf.base=.*|karaf.base=/opt/nexus-${NEXUS_VERSION}|g" \
+    -e "s|karaf.etc=.*|karaf.etc=/opt/nexus-${NEXUS_VERSION}/etc|g" \
+    -e "s|java.util.logging.config.file=.*|java.util.logging.config.file=/opt/nexus-${NEXUS_VERSION}/etc|g" \
+    -e "s|karaf.data=.*|karaf.data=${NEXUS_DATA}|g" \
+    -e "s|java.io.tmpdir=.*|java.io.tmpdir=${NEXUS_DATA}/tmp|g" \
     -i "/opt/nexus-${NEXUS_VERSION}/bin/nexus.vmoptions"
   mkdir -p "${NEXUS_DATA}"
   chown -R nexus "${NEXUS_DATA}"
