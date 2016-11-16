1a7dc7ff6d4ad8b91592e4577e66e0e32bff3229
cavemandaveman <cavemandaveman@openmailbox.org>
2016-11-16 11:07:47 -0600

    Update directory structure

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM alpine:3.4
 MAINTAINER cavemandaveman <cavemandaveman@openmailbox.org>
 
 ENV NEXUS_VERSION="3.0.1-01" \
+    SONATYPE_DIR="/opt/sonatype" \
+    NEXUS_HOME="${SONATYPE_DIR}/nexus" \
     NEXUS_DATA="/nexus-data" \
     JAVA_MIN_MEM="1200M" \
     JAVA_MAX_MEM="1200M" \
@@ -13,16 +15,17 @@ RUN set -x \
         openjdk8-jre-base \
         openssl \
         su-exec \
-    && mkdir "/opt" \
+    && mkdir -p "${SONATYPE_DIR}" \
     && wget -qO - "https://download.sonatype.com/nexus/3/nexus-${NEXUS_VERSION}-unix.tar.gz" \
-    | tar -zxC "/opt" \
+    | tar -zxC "${SONATYPE_DIR}" \
+    && mv "${SONATYPE_DIR}/nexus-${NEXUS_VERSION}" "${NEXUS_HOME}" \
     && adduser -S -h ${NEXUS_DATA} nexus
 
 EXPOSE 8081 8443
 
-WORKDIR "/opt/nexus-${NEXUS_VERSION}"
+WORKDIR "${NEXUS_HOME}"
 
-VOLUME ${NEXUS_DATA}
+VOLUME "${NEXUS_DATA}"
 
 COPY docker-entrypoint.sh /
 ENTRYPOINT ["/docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,30 +4,30 @@ set -x
 
 if [ "$1" == 'bin/nexus' ]; then
   if [ -f "${NEXUS_DATA}/keystore.jks" ]; then
-    mkdir -p "/opt/nexus-${NEXUS_VERSION}/etc/ssl"
-    ln -s "${NEXUS_DATA}/keystore.jks" "/opt/nexus-${NEXUS_VERSION}/etc/ssl/keystore.jks"
+    mkdir -p "${NEXUS_HOME}/etc/ssl"
+    ln -s "${NEXUS_DATA}/keystore.jks" "${NEXUS_HOME}/etc/ssl/keystore.jks"
     sed \
       -e "s|OBF.*|${JKS_PASSWORD}</Set>|g" \
-      -i "/opt/nexus-${NEXUS_VERSION}/etc/jetty-https.xml"
+      -i "${NEXUS_HOME}/etc/jetty-https.xml"
     sed \
       -e "s|nexus-args=.*|nexus-args=\${karaf.etc}/jetty.xml,\${karaf.etc}/jetty-http.xml,\${karaf.etc}/jetty-requestlog.xml,\${karaf.etc}/jetty-https.xml,\${karaf.etc}/jetty-http-redirect-to-https.xml|g" \
-      -i "/opt/nexus-${NEXUS_VERSION}/etc/org.sonatype.nexus.cfg"
+      -i "${NEXUS_HOME}/etc/org.sonatype.nexus.cfg"
     grep \
-      -q "application-port-ssl" "/opt/nexus-${NEXUS_VERSION}/etc/org.sonatype.nexus.cfg" || \
+      -q "application-port-ssl" "${NEXUS_HOME}/etc/org.sonatype.nexus.cfg" || \
       sed \
         -e "\|application-port|a\application-port-ssl=8443" \
-        -i "/opt/nexus-${NEXUS_VERSION}/etc/org.sonatype.nexus.cfg"
+        -i "${NEXUS_HOME}/etc/org.sonatype.nexus.cfg"
   fi
   sed \
     -e "s|-Xms.*|-Xms${JAVA_MIN_MEM}|g" \
     -e "s|-Xmx.*|-Xmx${JAVA_MAX_MEM}|g" \
-    -e "s|karaf.home=.*|karaf.home=/opt/nexus-${NEXUS_VERSION}|g" \
-    -e "s|karaf.base=.*|karaf.base=/opt/nexus-${NEXUS_VERSION}|g" \
-    -e "s|karaf.etc=.*|karaf.etc=/opt/nexus-${NEXUS_VERSION}/etc|g" \
-    -e "s|java.util.logging.config.file=.*|java.util.logging.config.file=/opt/nexus-${NEXUS_VERSION}/etc|g" \
+    -e "s|karaf.home=.*|karaf.home=${NEXUS_HOME}|g" \
+    -e "s|karaf.base=.*|karaf.base=${NEXUS_HOME}|g" \
+    -e "s|karaf.etc=.*|karaf.etc=${NEXUS_HOME}/etc|g" \
+    -e "s|java.util.logging.config.file=.*|java.util.logging.config.file=${NEXUS_HOME}/etc|g" \
     -e "s|karaf.data=.*|karaf.data=${NEXUS_DATA}|g" \
     -e "s|java.io.tmpdir=.*|java.io.tmpdir=${NEXUS_DATA}/tmp|g" \
-    -i "/opt/nexus-${NEXUS_VERSION}/bin/nexus.vmoptions"
+    -i "${NEXUS_HOME}/bin/nexus.vmoptions"
   mkdir -p "${NEXUS_DATA}"
   chown -R nexus "${NEXUS_DATA}"
   exec su-exec nexus "$@"
