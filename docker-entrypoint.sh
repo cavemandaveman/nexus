02ae3054749890435f1dc7240ab2119f51ae8681
cavemandaveman <cavemandaveman@openmailbox.org>
2017-08-23 16:23:45 -0500

    Update keystore password injection

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ if [ "$1" = 'bin/nexus' ]; then
   if [ -f "${NEXUS_DATA}/keystore.jks" ]; then
     ln -s "${NEXUS_DATA}/keystore.jks" "${NEXUS_HOME}/etc/ssl/keystore.jks"
     sed \
-      -e "s|OBF.*|${JKS_PASSWORD}</Set>|g" \
+      -e "s|password</Set>|${JKS_PASSWORD}</Set>|g" \
       -i "${NEXUS_HOME}/etc/jetty/jetty-https.xml"
     sed \
       -e "s|nexus-args=.*|nexus-args=\${jetty.etc}/jetty.xml,\${jetty.etc}/jetty-http.xml,\${jetty.etc}/jetty-requestlog.xml,\${jetty.etc}/jetty-https.xml,\${jetty.etc}/jetty-http-redirect-to-https.xml|g" \
